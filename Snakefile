ad845ee817cf120ff188de26e28d3be934b67d39
Ino de Bruijn <ino.debruijn@scilifelab.se>
2013-08-02 11:19:33 +0200

    Submitting with Snakefile-sbatch.py with proper dependencies still doesn't work. The test somehow stucks at creating ref.fa.bwt

--- Snakefile
+++ Snakefile
@@ -1,14 +1,24 @@
-
-
 # Split parameters
 SPLIT_OUT="split-out"
-LINES_PER_FILE=os.environ.get("LINES_PER_FILE", "4000000")
+LINES_PER_FILE=os.environ.get("LINES_PER_FILE", "400000000")
 
 # aln parameters
 MAP_IM_OUT="map-intermediate-out"
 
 # merge parameters
 MERGE_OUT="map"
+
+# picard location
+PICARD_DIR="/bubo/home/h16/inod/src/picard-tools-1.89"
+
+# Given there's a pair1.fastq and pair2.fastq, split up all reads
+rule all:
+    message: """(1) Splitting up pair1.fastq and pair2.fastq for parallelization (use --cluster)
+                (2) map them to ref.fa
+                (3) sort bam and get coverage info
+    """
+    input: MERGE_OUT + "/pair_ref-s-md-s.bam", MERGE_OUT + "/pair_ref-s-md-s.bam.bai",
+           MERGE_OUT + "/pair_ref-s-md-s.coverage"
 
 rule merge_all:
     input: MERGE_OUT + "/pair_ref.bam"
@@ -37,6 +47,7 @@ rule split2:
 rule bwt:
     input: "ref.fa"
     output: "ref.fa.bwt"
+    log: "ref.fa.bwt.sm.log"
     shell: """
 	bwa index {input}
     """
@@ -45,7 +56,8 @@ rule aln:
     input: pairn=SPLIT_OUT + "/pair{pair_nr}.fastq.split.{id}", refbwt="ref.fa.bwt", ref="ref.fa"
     threads: 8
     params: out_dir=MAP_IM_OUT
-    output: protected(MAP_IM_OUT + "/pair{pair_nr}.fastq.split.{id}.sai")
+    output: MAP_IM_OUT + "/pair{pair_nr}.fastq.split.{id}.sai"
+    log: MAP_IM_OUT + "/pair{pair_nr}.fastq.split.{id}.sai.sm.log"
     shell: """
     mkdir -p {params.out_dir}
 	bwa aln -t {threads} {input.ref} -{wildcards.pair_nr} {input.pairn} > {output}
@@ -54,7 +66,8 @@ rule aln:
 rule sampe:
     input: sai1=MAP_IM_OUT + "/pair1.fastq.split.{id}.sai", sai2=MAP_IM_OUT + "/pair2.fastq.split.{id}.sai", pair1=SPLIT_OUT + "/pair1.fastq.split.{id}", pair2=SPLIT_OUT + "/pair2.fastq.split.{id}", refbwt="ref.fa.bwt", ref="ref.fa"
     params: out_dir=MAP_IM_OUT
-    output: protected(MAP_IM_OUT + "/pair.fastq.split.{id}.sam")
+    output: MAP_IM_OUT + "/pair.fastq.split.{id}.sam"
+    log: MAP_IM_OUT + "/pair.fastq.split.{id}.sam.sm.log"
     shell: """
     mkdir -p {params.out_dir}
 	bwa sampe {input.ref} {input.sai1} {input.sai2} {input.pair1} {input.pair2} > {output}
@@ -63,6 +76,7 @@ rule sampe:
 rule samtobam:
     input: "{name}.sam"
     output: "{name}.bam"
+    log: "{name}.bam.sm.log"
     shell: """
     samtools view -bS {input} > {output}
     """
@@ -71,9 +85,88 @@ rule merge:
     input: dynamic(MAP_IM_OUT + "/pair.fastq.split.{splitid}.bam")
     params: out_dir=MERGE_OUT
     output: MERGE_OUT + "/pair_ref.bam"
+    log: MERGE_OUT + "/pair_ref.bam.sm.log"
     shell: """
     mkdir -p {params.out_dir}
     samtools merge {output} {input}
+    """
+
+rule sort:
+    input: "{name}.bam"
+    output: "{name}-s.bam"
+    log: "{name}-s.bam.sm.log"
+    params: srtsam_jar=PICARD_DIR + "/SortSam.jar"
+    shell: """
+    java -jar {params.srtsam_jar} \
+        INPUT={input} \
+        OUTPUT={output} \
+        SORT_ORDER=coordinate \
+        VALIDATION_STRINGENCY=SILENT
+    """
+
+# Removes reads mapping to the exact same location, probably duplicates
+rule removeduplicates:
+    input: "{name}-s.bam"
+    output: "{name}-s-md.bam", "{name}-s-md.metrics"
+    log: "{name}-s-md.bam.sm.log"
+    params: mrkdup_jar=PICARD_DIR + "/MarkDuplicates.jar"
+    shell: """
+    java -jar {params.mrkdup_jar} \
+        INPUT={input} \
+        OUTPUT={output[0]} \
+        METRICS_FILE={output[1]} \
+        MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000 \
+        REMOVE_DUPLICATES=TRUE \
+        VALIDATION_STRINGENCY=SILENT
+    """
+
+# Gets rid of the MAPQ should be 0 warning
+rule cleansam:
+    input: "{name}.bam"
+    output: "{name}-c.bam"
+    log: "{name}-c.bam.sm.log"
+    params: clnsam_jar=PICARD_DIR + "/CleanSam.jar"
+    shell: """
+    java -jar {params.clnsam_jar} \
+        INPUT={input} \
+        OUTPUT={output} \
+        QUIET=TRUE \
+    """
+
+# Create index file for fast bam access
+rule index:
+    input: "{name}.bam"
+    output: "{name}.bam.bai"
+    log: "{name}.bam.bai.sm.log"
+    shell: """
+    samtools index {input}
+    """
+
+# Use BEDTools to get coverage of bam file
+rule coverage:
+    input: "{name}.bam"
+    output: "{name}.coverage"
+    shell: """
+    genomeCoverageBed -ibam {input} > {output}
+    """
+
+# Calculate mean coverage per contig given BEDTools output
+# TODO: doesn't work, {} should be escaped somehow
+rule mean_coverage_per_contig:
+    input: "{name}.coverage"
+    output: "{name}.mean.coverage.per.contig"
+    shell: """
+    awk 'BEGIN {pc=""}
+    {
+        c=$1;
+        if (c == pc) {
+            cov=cov+$2*$5;
+        } else {
+            print pc,cov;
+            cov=$2*$5;
+            pc=c
+        }
+    } END {print pc,cov}' {input} | tail -n +2 > {output}
     """
 
 rule clean:
--- Snakefile-sbatch.py
+++ Snakefile-sbatch.py
@@ -3,19 +3,36 @@ import sys
 import subprocess
 import os
 import math
+import ipdb
+import errno
+
+
+def make_dir(directory):
+    """Make directory unless existing. Ignore error in the latter case."""
+    try:
+        os.makedirs(directory)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 
 class SnakeJob:
     """Snakemake can generate bash scripts that can be sumbitted by a
     scheduler.  This class reads the bash script and stores the number of the
     rule, name of bash file and the supplied input files."""
-    def __init__(self, snakebashfile):
+    def __init__(self, snakebashfile, dependencies=None):
         fh = open(snakebashfile, 'r')
         self.scriptname = snakebashfile
         fh.readline()
         self.rule = fh.readline().split()[1]
         self.ifiles = fh.readline().split()[1:]
         self.ofiles = fh.readline().split()[1:]
+        if dependencies == None:
+            self.dependencies = None
+        else:
+            # expects snakemake like string
+            self.dependencies = dependencies.split(',')
+            assert len(self.dependencies) >= 1
         fh.close()
 
 
@@ -27,25 +44,38 @@ class UndefinedJobRule(Exception):
 
 
 class SnakeJobSbatch(SnakeJob):
+    def __init__(self, snakebashfile, dependencies=None):
+        SnakeJob.__init__(self, snakebashfile, dependencies)
+        if self.dependencies == None:
+            self.dep_str = ''
+        else:
+            self.dep_str = '-d ' + ','.join(["afterok:%s" % d for d in self.dependencies])
+
     def schedule(self):
         """Schedules a snakemake job with sbatch and determines resource usage
         based on input files."""
+        # create the output directory, so slurm output can go there
+        make_dir(os.path.dirname(os.path.abspath(self.ofiles[0])))
+
         if self.rule == 'split1' or self.rule == 'split2' or self.rule == 'bwt':
             #hours = math.ceil(os.path.getsize(self.ifiles[0]) / float(500 * 1024 ** 2))
-            hours = 1
-            sbatch_cmd = 'sbatch -A b2010008 -p core -t %i:00:00 --output=snakemake-%%j.out -J %s ~/bin/sbatch_job bash %s' % (hours, self.rule, self.scriptname)
-        elif self.rule == 'aln':
-            hours = 1
-            sbatch_cmd = 'sbatch -A b2010008 -p core -t %i:00:00 --output=snakemake-%%j.out -J %s ~/bin/sbatch_job bash %s' % (hours, self.rule, self.scriptname)
+            minutes = 15
+            sbatch_cmd = 'sbatch %s -A b2010008 -p core --qos=short -t 00:%i:00 --output=%s-slurm-%%j.out -J %s ~/bin/sbatch_job bash %s' % (self.dep_str, minutes, self.ofiles[0], self.rule, self.scriptname)
+        elif self.rule in ['aln', 'sort']:
+            minutes = 15
+            sbatch_cmd = 'sbatch %s -A b2010008 -p node --qos=short -t 00:%i:00 --output=%s-slurm-%%j.out -J %s ~/bin/sbatch_job bash %s' % (self.dep_str, minutes, self.ofiles[0], self.rule, self.scriptname)
         elif self.rule == 'sampe':
-            hours = 1
-            sbatch_cmd = 'sbatch -A b2010008 -p core -t %i:00:00 --output=snakemake-%%j.out -J %s ~/bin/sbatch_job bash %s' % (hours, self.rule, self.scriptname)
+            minutes = 15
+            sbatch_cmd = 'sbatch %s -A b2010008 -p core --qos=short -t 00:%i:00 --output=%s-slurm-%%j.out -J %s ~/bin/sbatch_job bash %s' % (self.dep_str, minutes, self.ofiles[0], self.rule, self.scriptname)
         elif self.rule == 'samtobam':
-            hours = 1
-            sbatch_cmd = 'sbatch -A b2010008 -p core -t %i:00:00 --output=snakemake-%%j.out -J %s ~/bin/sbatch_job bash %s' % (hours, self.rule, self.scriptname)
+            minutes = 15
+            sbatch_cmd = 'sbatch %s -A b2010008 -p core --qos=short -t 00:%i:00 --output=%s-slurm-%%j.out -J %s ~/bin/sbatch_job bash %s' % (self.dep_str, minutes, self.ofiles[0], self.rule, self.scriptname)
         elif self.rule == 'merge':
-            hours = 1
-            sbatch_cmd = 'sbatch -A b2010008 -p core -t %i:00:00 --output=snakemake-%%j.out -J %s ~/bin/sbatch_job bash %s' % (hours, self.rule, self.scriptname)
+            minutes = 15
+            sbatch_cmd = 'sbatch %s -A b2010008 -p core --qos=short -t 00:%i:00 --output=%s-slurm-%%j.out -J %s ~/bin/sbatch_job bash %s' % (self.dep_str, minutes, self.ofiles[0], self.rule, self.scriptname)
+        elif self.rule in ['removeduplicates', 'cleansam', 'index', 'coverage', 'mean_coverage_per_contig']:
+            minutes = 15
+            sbatch_cmd = 'sbatch %s -A b2010008 -p core --qos=short -t 00:%i:00 --output=snakemake-%%j.out -J %s ~/bin/sbatch_job bash %s' % (self.dep_str, minutes, self.ofiles[0], self.rule, self.scriptname)
         elif self.rule in ['split_all', 'clean', 'all', 'merge_all', 'test']:
             # no scheduling just run
             sbatch_cmd = 'bash %s' % (self.scriptname)
@@ -54,11 +84,18 @@ class SnakeJobSbatch(SnakeJob):
             return 2
 
         print(sbatch_cmd)
-        subprocess.Popen(sbatch_cmd, shell=True)
+        popenrv = subprocess.Popen(sbatch_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()
+        print("%i" % int(popenrv[0].split()[-1]))
 
 
 if __name__ == '__main__':
-    sj = SnakeJobSbatch(sys.argv[1])
+
+    if len(sys.argv) == 2:
+        sj = SnakeJobSbatch(sys.argv[1])
+    elif len(sys.argv) == 3:
+        sj = SnakeJobSbatch(sys.argv[1], sys.argv[2])
+    else:
+        raise Exception('Expected snakemake arguments, should max be 3')
     try:
         sj.schedule()
     except UndefinedJobRule as err:
